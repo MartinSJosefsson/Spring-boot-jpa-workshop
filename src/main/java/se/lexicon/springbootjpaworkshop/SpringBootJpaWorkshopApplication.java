4301143b8ac5f2d80d13c822563cf09191cc3586
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-06-09 15:08:24 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/springbootjpaworkshop/SpringBootJpaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.springbootjpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaWorkshopApplication.class, args);
+    }
+
+}
