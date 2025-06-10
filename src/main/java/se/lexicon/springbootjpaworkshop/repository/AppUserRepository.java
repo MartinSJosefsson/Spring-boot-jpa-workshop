3764efd69e52caf33deee1847593eb0039a389fd
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-06-10 10:27:57 +0200

    Repositories

--- src/main/java/se/lexicon/springbootjpaworkshop/repository/AppUserRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop/repository/AppUserRepository.java
@@ -1,7 +1,9 @@
 package se.lexicon.springbootjpaworkshop.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.lexicon.springbootjpaworkshop.entity.AppUser;
 
+@Repository
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 }
--- src/main/java/se/lexicon/springbootjpaworkshop/repository/DetailsRepository.java
+++ src/main/java/se/lexicon/springbootjpaworkshop/repository/DetailsRepository.java
@@ -1,7 +1,9 @@
 package se.lexicon.springbootjpaworkshop.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.lexicon.springbootjpaworkshop.entity.Details;
 
+@Repository
 public interface DetailsRepository extends CrudRepository<Details, Integer> {
 }
