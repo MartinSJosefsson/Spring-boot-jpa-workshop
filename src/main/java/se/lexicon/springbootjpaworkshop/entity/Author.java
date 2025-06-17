4d5431b110cd8e116533fc19b3b2e330d3ddb31a
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-06-17 09:11:50 +0200

    Part 4 first commit

---
+++ src/main/java/se/lexicon/springbootjpaworkshop/entity/Author.java
@@ -0,0 +1,7 @@
+package se.lexicon.springbootjpaworkshop.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Author {
+}
---
+++ src/main/java/se/lexicon/springbootjpaworkshop/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package se.lexicon.springbootjpaworkshop.repository;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AuthorRepository {
+}
